3758126ce783c6d827b01714c9dbf43862681afa
SagarNegi10 <sagarnegi13.0@gmail.com>
2025-05-12 12:43:57 +0530

    Initialize backend dependencies

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import connectDB from './config/mongodb.js'
+import connectCloudinary from './config/cloudinary.js'
+
+// App config
+
+const app = express()
+const port = process.env.PORT || 4000
+connectDB()
+connectCloudinary()
+
+// Middlewares
+
+app.use(express.json())
+app.use(cors())
+
+// Api endpoitns
+
+app.get('/',(req,res)=>{
+    res.send("API Working")
+})
+
+app.listen(port, ()=>console.log('Server Started on port: '+port))
