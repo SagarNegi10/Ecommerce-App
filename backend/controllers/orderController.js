cf22109fb01a12d39e853d6756f34573f52f3ebf
SagarNegi10 <sagarnegi13.0@gmail.com>
2025-05-17 21:31:11 +0530

    Integrate cart controller endpoints with frontend logic

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,33 @@
+
+
+// Placing order using COD
+const placeOrder = async (req, res) => {
+
+}
+
+// Placing order using Stripe
+const placeOrderStripe = async (req, res) => {
+
+}
+
+// Placing order using Razorpay
+const placeOrderRazorpay = async (req, res) => {
+
+}
+
+// All orders data for admin panel
+const allOrder = async (req, res) => {
+
+}
+
+// User orders data for frontend
+const userOrder = async (req, res) => {
+
+}
+
+// Update order status from admin panel
+const updateStatus = async (req, res) => {
+
+}
+
+export {placeOrder, placeOrderStripe, placeOrderRazorpay, allOrder, userOrder, updateStatus}
